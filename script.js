b5a0444cc8006fcaf47e77195555b9b7141d3910
ljx_3954 <105200496+claim-12@users.noreply.github.com>
2024-09-14 18:50:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const coll = document.querySelectorAll(".collapsible");
+
+  coll.forEach(button => {
+    button.addEventListener("click", function() {
+      this.classList.toggle("active");
+
+      const content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+});
